2ffdab39ab9a92edcc617267fc8451bc9db1a818
ryannordeen <ryannordeen@gmail.com>
2020-08-27 21:02:36 -0500

    wibble

--- v4l2tricks/stream.py
+++ v4l2tricks/stream.py
@@ -10,6 +10,5 @@ def stream_media( fname, dev ='/dev/video1' ):
     Notes:  Per the producer section of the v4l2loopback wiki.
     ffmpeg -re -i "{0}" -f v4l2 "{1}"
     '''
+    return StreamProcess( fname, dev )
 
-    stream = StreamProcess( fname, dev )
-    return stream
