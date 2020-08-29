6e86a283328921fdaa9ae08b4758dba0a85d309b
Ryan Nordeen <ryannordeen@gmail.com>
2020-08-28 19:10:50 -0500

    Fixed blocking

--- v4l2tricks/ffmpeg_if.py
+++ v4l2tricks/ffmpeg_if.py
@@ -5,32 +5,64 @@
 import os
 import sys
 import ffmpeg
+from threading import Thread
+import pdb
+
+try:
+    from queue import Queue, Empty
+except ImportError as e:
+    # Python 2.x
+    from Queue import Queue, Empty
+
+def enqueue_output( process, queue ):
+    try:
+        outs, errs = process.communicate()
+    except Exception:
+        proc.kill()
 
 class StreamProcess( object ):
     '''
     StreamProcess
+
+https://github.com/kkroening/ffmpeg-python/issues/156#issuecomment-449553709
     '''
     def __init__( self, fname, device = '/dev/video20', verbose = True ):
         super( StreamProcess, self ).__init__()
+        self._q = Queue()
+
         self._verbose = verbose
         if verbose: print( 'Attempting to Stream: {} to {}'.format( fname, device ) )
         width, height, num_frames = probe( fname )
         if verbose: print( '{0}: w={1}, h={2}'.format( fname, width, height ) )
+
+        # Create ffmpeg interface process
         inp = ffmpeg.input( fname, re=None, ).output( device, f='v4l2' )
         if verbose: print( inp.compile() )
 
         process = (
-            inp.run_async( pipe_stdout      = True,
-                           pipe_stdin       = True,
-                           quiet            = False,#True,
+            inp.run_async( pipe_stdout      = False,#True,
+                           pipe_stdin       = False,#True,
+                           quiet            = True,
                            overwrite_output = True,
             )
         )
+        print type( process )
         self._proc = process
-    
+
+        self._t    = Thread( target = enqueue_output, args = (process, self._q ) )
+        self._t.deamon = True # Thread must die with the program
+        self._t.start()
+
+
     @property
     def readline( self ):
-        return self._proc.stdout.readline()
+    #    return self._proc.stdout.readline()
+        try:
+            line = self._q.get_nowait() # or q.get(timeout=.1)
+        except Empty:
+            return None
+        return line
+
 
     def read( self, bufsize = 80 ):
         self._output += self._proc.stdout.read( bufsize )
@@ -42,6 +74,7 @@ class StreamProcess( object ):
 
     def stop( self ):
         self._proc.kill()
+        self._t.join()
 
     def dump( self ):
         for line in self.readline:
