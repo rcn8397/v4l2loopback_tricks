f6120976342134db34743fd5a5b0714b32f77708
Ryan Nordeen <ryannordeen@gmail.com>
2021-01-14 14:31:07 -0600

    Fixed some issues with monitor

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 '''
 Video tutorial
 '''
@@ -8,20 +8,33 @@ import pdb
 import numpy as np
 import cv2
 
+WINDOW_NAME   = 'Monitor'
+SCALE_PERCENT = 50
+
 def main( args ):
+    cv2.namedWindow( WINDOW_NAME, cv2.WINDOW_NORMAL )
     cap = cv2.VideoCapture( args.device )
     while( cap.isOpened() ):
         # Capture frame-by-frame
         ret, frame, = cap.read()
 
         # Operates on the frame come here
-        gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
+        frame = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
+        
+        width  = int( frame.shape[ 1 ] * SCALE_PERCENT / 100 )
+        height = int( frame.shape[ 0 ] * SCALE_PERCENT / 100 )
+        dsize = ( width, height )
 
+        output = cv2.resize( frame, dsize )
+        
         # Display the resulting frame
-        cv2.imshow( 'Monitor', gray )
+        cv2.imshow( WINDOW_NAME, output )
         if cv2.waitKey( 1 ) & 0xff == ord( 'q' ):
             break
 
+#       if cv2.getWindowProperty( WINDOW_NAME, cv2.WND_PROP_VISIBLE ) <1:
+#           break;
+        
     # When everything is done, release the capture device
     cap.release()
     cv2.destroyAllWindows()
